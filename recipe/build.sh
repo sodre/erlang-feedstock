7290246d96a5cc7be3404de92b5a0b190d697834
Patrick Sodré <psodre@gmail.com>
2019-08-18 14:33:45 +0000

    enable linux tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,22 +11,9 @@ export ERL_TOP="$(pwd)"
     --enable-m${ARCH}-build
 make -j $CPU_COUNT
 
-# FIXME
-#
-# The tests appear to block upload on CircleCI.
-# So we skip them on the Linux build. It would
-# be nice to be able to run the tests again on
-# Linux. Best guess is we might be missing
-# some dependencies. See the linked issue.
-#
-# https://github.com/conda-forge/erlang-feedstock/issues/1
-#
-if [ "$(uname)" == "Darwin" ]
-then
-    make release_tests
-    cd "${ERL_TOP}/release/tests/test_server"
-    ${ERL_TOP}/bin/erl -s ts install -s ts smoke_test batch -s init stop
-    cd ${ERL_TOP}
-fi
+make release_tests
+cd "${ERL_TOP}/release/tests/test_server"
+${ERL_TOP}/bin/erl -s ts install -s ts smoke_test batch -s init stop
+cd ${ERL_TOP}
 
 make install
